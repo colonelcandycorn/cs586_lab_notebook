a3cab02372d2f8ef36afcbce80fa2544483debf9
Sarah Dylan <SarahmDylan@gmail.com>
2024-05-18 22:13:30 -0700

    Finish lab 7

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -3,7 +3,15 @@
 
 // fetch JavaScript objects representing specific elements in the DOM
 
+const userInput1 = document.querySelector("#userInput1");
+const copyButton = document.querySelector("#copy");
+const output = document.querySelector(".output");
+
 // add an event listener on the target element
+
+copyButton.addEventListener("click", () => {
+  output.textContent = userInput1.value;
+});
 
 // callback function to handle event
 
@@ -12,6 +20,16 @@
 
 // fetch JavaScript objects representing specific elements in the DOM
 
+const inputEventExample = document.querySelector("#inputEventExample");
+const userInput2 = document.querySelector("#userInput2");
+const output2 = document.createElement("div");
+output2.classList.add("output2");
+
 // add an event listener on the target element
+
+userInput2.addEventListener("input", () => {
+  output2.textContent = userInput2.value;
+  inputEventExample.appendChild(output2);
+});
 
 // callback function to handle event
--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -1,9 +1,31 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const loading = document.querySelector("#loading");
+      loading.style.display = "none";
+      data.forEach((book) => {
+        const bookElement = document.createElement("div");
+        // get year from the release date
+        const year = new Date(book.released);
+        bookElement.classList.add("text-center", "book"); // already inherited from the parent but just to satisfy the requirements
+        bookElement.innerHTML = `
+          <h2>${book.name}</h2>
+          <p>Author: ${book.authors}</p>
+          <p>Publication Year: ${year.getUTCFullYear()}</p>
+          <p>Number of Pages: ${book.numberOfPages}</p>
+        `;
+        app.appendChild(bookElement);
+      });
+    });
 };
+
+fetchData(url);
